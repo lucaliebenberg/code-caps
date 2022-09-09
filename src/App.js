05f7916333ecd6c711c308936fda392947b4ed08
Luca Liebenberg <liebenbergluca@gmail.com>
2022-09-09 13:33:04 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <HeroSection />
+      <Footer />
     </div>
   );
 }
